4dfd560e7ccd853a68fb386c6f7d82d3952bf058
SamVega <sam.vega@tuta.io>
2021-01-10 21:48:03 +0100

    theme changes

--- shiva/theme.lua
+++ shiva/theme.lua
@@ -8,7 +8,7 @@ local dpi = require("beautiful.xresources").apply_dpi
 -- {{{ Main
 local theme = {}
 theme.confdir = os.getenv("HOME") .. "/.config/awesome/themes/shiva"
-theme.wallpaper = theme.confdir .. "/ShivaPython.jpg"
+theme.wallpaper = theme.confdir .. "/Shivasky.jpg"
 -- }}}
 
 -- {{{ Styles
