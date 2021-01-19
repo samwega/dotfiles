be71963569497fa3d5177984deb2422bb20c3c9e
SamVega <sam.vega@tuta.io>
2021-01-19 16:53:14 +0100

    -

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,7 @@ starship init fish | source
 # set fish_greeting "🐟𓂀 𓆟 𓆞  "
 set fish_greeting "𓂀 "
 # set fish_greeting "𓆟 "
+
+set -x CDPATH /home/samvega/Repos/gitlab.com/samwega/
+
+set -x CDPATH /home/samvega/Projects/Python/
