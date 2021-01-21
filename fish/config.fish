4d019af9d0c8ed36c9ea6b148a8a9fab8d6de079
SamVega <sam.vega@tuta.io>
2021-01-21 02:08:58 +0100

    -

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,16 @@
 
 starship init fish | source
 
+set -x CDPATH /home/samvega/Repos/samwega/ .
+
 # set fish_greeting "🐟𓂀 𓆟 𓆞  "
 # set fish_greeting "𓂀 "
-set fish_greeting "<><"
-
-set -x CDPATH /home/samvega/Repos/samwega/ .
+# set fish_greeting "<>꧂  ะ"
+# set fish_greeting "꧁ ༺ ༒ ༻ ꧂ "
+# set fish_greeting "♛"
+# set fish_greeting "※ ะ"
+# set fish_greeting "๛"
+# set fish_greeting "᳀ ༫ ࿂ ࿆༗ ₷ Ꮠ ℟"
+# set fish_greeting "࿇"
+set fish_greeting "༺  ♛ ༻"
 
